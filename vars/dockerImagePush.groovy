f05bcd91946db12935333554e9f4497376f493d7
hasmita-patnana7 <148422141+hasmita-patnana7@users.noreply.github.com>
2024-04-22 11:32:33 +0530

    Update code dockerImagePush.groovy

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -10,11 +10,3 @@ def call(String project, String ImageTag, String hubUser){
     sh "docker image push ${hubUser}/${project}:latest"   
 }
 
-
-// def call(String aws_account_id, String region, String ecr_repoName){
-    
-//     sh """
-//      aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-//      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-//     """
-// }
