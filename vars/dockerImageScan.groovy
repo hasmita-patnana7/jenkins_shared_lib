8db187b1802a223c02fe9f7580789fe949660847
hasmita-patnana7 <148422141+hasmita-patnana7@users.noreply.github.com>
2024-04-30 11:31:39 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,3 +1,10 @@
+/**
+ * This method performs a vulnerability scan on a Docker image.
+ * @param project The name of the project for which the Docker image is scanned.
+ * @param ImageTag The tag of the Docker image to be scanned.
+ * @param hubUser The username name on Docker Hub.
+ */
+
 def call(String project, String ImageTag, String hubUser){
     
     sh """   
