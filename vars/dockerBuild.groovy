1eb724f9045701b5f8ef944f38872ea9f956159c
hasmita-patnana7 <148422141+hasmita-patnana7@users.noreply.github.com>
2024-04-30 11:25:01 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,10 @@
+/**
+ * This method builds a Docker image for a given project.
+ * @param project The name of the project for which the Docker image is built.
+ * @param ImageTag The tag to be applied to the Docker image.
+ * @param hubUser The username or organization name on Docker Hub.
+ */
+
 def call(String project, String ImageTag, String hubUser){
     
     sh """
@@ -5,10 +12,4 @@ def call(String project, String ImageTag, String hubUser){
     """
 }
 
-// def call(String aws_account_id, String region, String ecr_repoName){
-    
-//     sh """
-//      docker build -t ${ecr_repoName} .
-//      docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-//     """
-// }
+
