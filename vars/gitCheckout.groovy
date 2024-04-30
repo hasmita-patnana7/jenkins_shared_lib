38f9a3418b29e0e9b30b707f04b4c99da87689bb
hasmita-patnana7 <148422141+hasmita-patnana7@users.noreply.github.com>
2024-04-30 11:36:59 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,8 +1,13 @@
+/**
+ * Performs a Git checkout of a specific branch from a Git repository.
+ * @param stageParams A map containing parameters for the Git checkout stage.
+ *                    Contains the keys 'branch' and 'url'.
+ */
 def call(Map stageParams) {
- 
+    // Using the Jenkins pipeline 'checkout' step to perform Git checkout
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.branch ]],
         userRemoteConfigs: [[ url: stageParams.url ]]
     ])
-  }
+}
