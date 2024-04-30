cc791e9cd22f0ecaa271989c159ab576d15ae9db
hasmita-patnana7 <148422141+hasmita-patnana7@users.noreply.github.com>
2024-04-30 11:48:10 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,3 +1,10 @@
+/**
+ * This method performs static code analysis using SonarQube.
+ * It executes the Maven command 'mvn clean package sonar:sonar' within a SonarQube environment.
+ * This command performs a clean build, packages the application, and runs SonarQube analysis
+ * @param credentialsId The ID of the SonarQube credentials configured in Jenkins.
+ */
+
 def call(credentialsId){
 
     withSonarQubeEnv(credentialsId: credentialsId) {
